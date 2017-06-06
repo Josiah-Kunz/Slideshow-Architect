389e3e3350991baa69c96e4897ba8c6d190f2ab4
JosiahKunz <jdkunz@anderson.edu>
2017-06-05 21:41:27 -0400

    started bg

---
+++ Slideshow Architect/Assets/Resources/Scripts/CustomAssetImporter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class TexturePostProcessor : AssetPostprocessor{
+	
+	void OnPostprocessTexture(Texture2D texture){
+		TextureImporter importer = assetImporter as TextureImporter;
+		importer.textureType = TextureImporterType.Sprite;
+		importer.spriteImportMode = SpriteImportMode.Single;
+		importer.wrapMode = TextureWrapMode.Clamp;
+
+		Object asset = AssetDatabase.LoadAssetAtPath (importer.assetPath, typeof(Texture2D));
+		if (asset) {
+			EditorUtility.SetDirty (asset);
+		} else {
+			texture.wrapMode = TextureWrapMode.Clamp;
+		} 
+	}
+
+}
